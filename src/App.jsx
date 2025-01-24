c378686c69498126be31bd10002a8822f4d898ec
99ShinByeongseon <sun6988shin@naver.com>
2025-01-24 13:03:20 +0900

    🩹 fix : App.jsx 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import { EmptyText } from './components/EmptyText';
 import { MedalForm } from './components/MedalForm';
 import { MedalList } from './components/MedalList';
 
 function App() {
+  const [medals, setMedals] = useState([]);
+
+  const handleAdd = (newItem) => {
+    setMedals((prev) => [...prev, newItem]);
+  };
+
+  const handleDelete = (country) => {
+    setMedals((prev) => prev.filter((item) => item.country !== country));
+  };
+
   return (
-    <>
-      <MedalForm />
-      <EmptyText />
-      <MedalList />
-    </>
+    <div className='container'>
+      <MedalForm onAdd={handleAdd} />
+      <EmptyText isEmpty={medals.length === 0} />
+      <MedalList medals={medals} onDelete={handleDelete} />
+    </div>
   );
 }
 
